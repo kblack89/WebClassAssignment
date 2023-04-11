db1e2642648afce35796959e50a7fcf698d88fd7
Kyle Black <kdavidblack89@gmail.com>
2023-04-11 12:44:05 +1200

    adding a few placeholder divs

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,21 +1,27 @@
+<script>
+
+  import { redirect,} from "@sveltejs/kit";
+  import { blank_object,} from "svelte/internal";
+
+</script>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
 body {
-  margin: 0;
+    margin: 0;
   font-family: Arial, Helvetica, sans-serif;
 }
 
 .topnav {
   overflow: hidden;
-  background-color: #333;
+  background-color: black;
 }
 
 .topnav a {
   float: left;
-  color: #f2f2f2;
+  color: white;
   text-align: center;
   padding: 14px 16px;
   text-decoration: none;
@@ -23,29 +29,39 @@ body {
 }
 
 .topnav a:hover {
-  background-color: #ddd;
+  background-color: white;
   color: black;
 }
-
-.topnav a.active {
-  background-color: #04AA6D;
+footer {
+  background-color: #779;
+  padding: 100px;
+  text-align: center;
   color: white;
 }
+
+.topcontent {
+  background-color: #777;
+  padding: 800px;
+  text-align: center;
+  color: white;
+}
+
+
 </style>
 </head>
 <body>
 
 <div class="topnav">
-  <a class="active" href="#page.svelte">Home</a>
+  <a href="#page.svelte">Home</a>
   <a href="#news">News</a>
   <a href="#contact">Contact</a>
   <a href="#about.svelte">About</a>
 </div>
 
-<div style="padding-left:16px">
-  <h2>Top Navigation Example</h2>
-  <p>Some content..</p>
+<div class="topcontent">
 </div>
+<footer>
 
+</footer>
 </body>
 </html>
