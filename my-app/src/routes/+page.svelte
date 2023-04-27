4dd2b5b9a1d67c7b54c24e5077b0b0d69f028584
Kyle Black <kdavidblack89@gmail.com>
2023-04-28 11:40:15 +1200

    Adding placeholder nav bar and filler main content

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -39,13 +39,32 @@ footer {
   color: white;
 }
 
+/* Column container */
+.row {  
+  display: flex;
+  flex-wrap: wrap;
+}
+/* Create two unequal columns that sits next to each other */
+/* Sidebar/left column */
+.side {
+  flex: 10%;
+  background-color: #f1f1f1;
+  padding: 20px;
+}
+
+
 .topcontent {
+  flex: 70%;
   background-color: #777;
-  padding: 800px;
+  padding: 80px;
   text-align: center;
   color: white;
 }
-
+.fakeimg {
+  background-color: #aaa;
+  width: 100px;
+  padding: 20px;
+}
 
 </style>
 </head>
@@ -57,9 +76,33 @@ footer {
   <a href="#contact">Contact</a>
   <a href="#about.svelte">About</a>
 </div>
-
-<div class="topcontent">
+<div class="row">
+<div class="side">
+  <h2>About Me</h2>
+  <h5>Photo of me:</h5>
+  <div class="fakeimg" style="height:100px;">Image</div>
+  <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+  <h3>More Text</h3>
+  <p>Lorem ipsum dolor sit ame.</p>
+  <div class="fakeimg" style="height:60px;">Image</div><br>
+  <div class="fakeimg" style="height:60px;">Image</div><br>
+  <div class="fakeimg" style="height:60px;">Image</div>
 </div>
+<div class="topcontent">
+  <h2>TITLE HEADING</h2>
+  <h5>Title description, Dec 7, 2017</h5>
+ 
+  <p>Some text..</p>
+  <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+  <br>
+  <h2>TITLE HEADING</h2>
+  <h5>Title description, Sep 2, 2017</h5>
+  <div class="fakeimg" style="height:200px;">Image</div>
+  <p>Some text..</p>
+  <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+</div>
+</div>
+
 <footer>
 
 </footer>
