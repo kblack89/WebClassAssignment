a45b34bab0ef51ab8dea3b516efa6d1d5ad9bd3b
Kyle Black <kdavidblack89@gmail.com>
2023-05-13 12:23:35 +1200

    Adding componant for slideshow gallery at top of main page

---
+++ my-app/src/lib/GallerySlideshow.svelte
@@ -0,0 +1,35 @@
+<script>
+ export const GalleryTop = null; 
+  </script>
+  
+<div>
+    <GalleryTop>
+    <img 
+     class ="gallery-image"
+     src="images/arcade.jpg"alt="First"/>
+     <img 
+     class ="gallery-image"
+     src="images/books.jpg"alt="Second"/>
+     <img 
+     class ="gallery-image"
+     src="images/chalkboard.jpg"alt="Third"/>
+     <img 
+     class ="gallery-image"
+     src="images/tardis.jpg"alt="Fourth"/>
+     <img 
+     class ="gallery-image"
+     src="images/controller.jpg"alt="Fifth"/>
+     <img 
+     class ="gallery-image"
+     src="images/desk-trooper.jpg"alt="Sixth"/>
+     <img 
+     class ="gallery-image"
+     src="images/headphones.jpg"alt="Seventh"/>
+     <img 
+     class ="gallery-image"
+     src="images/keyboard.jpg"alt="Eighth"/>
+     <img 
+     class ="gallery-image"
+     src="images/pc.jpg"alt="Ninth"/>
+    </GalleryTop>
+    </div>
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -15,39 +15,20 @@
   </div>
 
  <div class = "general-section">
-  <br>
-  <br>
-  <br>
-  <br>
+<h1>Start</h1>
   <!--This section will display an image gallery that moves right when scrolled up and left when scrolled right.Content will be images from my room-->
- <img 
- class ="gallery-image"
- src="images/arcade.jpg"/>
- <img 
- class ="gallery-image"
- src="images/books.jpg"/>
- <img 
- class ="gallery-image"
- src="images/chalkboard.jpg"/>
- <img 
- class ="gallery-image"
- src="images/tardis.jpg"/>
- <img 
- class ="gallery-image"
- src="images/controller.jpg"/>
- <img 
- class ="gallery-image"
- src="images/desk-trooper.jpg"/>
- <img 
- class ="gallery-image"
- src="images/headphones.jpg"/>
- <img 
- class ="gallery-image"
- src="images/keyboard.jpg"/>
- <img 
- class ="gallery-image"
- src="images/pc.jpg"/>
+ 
+  <script>
 
+  import { GalleryTop } from '$lib/GallerySlideshow.svelte';
+
+
+   export let GalleryTop; 
+  </script>
+  
+  <GalleryTop />
+
+  <h1>End</h1>
 </div> 
 
 <div class = "general-section">
