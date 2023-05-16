330f347334d104e5d1966287d44f3427337aeea3
Kyle Black <kdavidblack89@gmail.com>
2023-05-16 13:52:12 +1200

    copy card code and make three of them vertically in div

--- my-app/src/lib/card-flip.svelte
+++ my-app/src/lib/card-flip.svelte
@@ -10,6 +10,30 @@
         </div>
     </div>
 </div>
+<div class ="flipping-card">
+    <div class ="flipping-card-inner">
+        <div class ="flipping-card-front">
+            <p class ="title">Hi</p>
+            <p>hover over</p>
+        </div>
+        <div class ="flipping-card-back">
+            <p class ="title">Bye</p>
+            <p>See ya later!</p>
+        </div>
+    </div>
+</div>
+<div class ="flipping-card">
+    <div class ="flipping-card-inner">
+        <div class ="flipping-card-front">
+            <p class ="title">Hi</p>
+            <p>hover over</p>
+        </div>
+        <div class ="flipping-card-back">
+            <p class ="title">Bye</p>
+            <p>See ya later!</p>
+        </div>
+    </div>
+</div>
 
 <style>
 
