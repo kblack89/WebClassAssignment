b63f0a4e1d39ef8dcd0948d0aab58e9abcbc8b09
Kyle Black <kdavidblack89@gmail.com>
2023-05-22 10:11:07 +1200

    add come-into-view html

---
+++ my-app/src/lib/come-into-view.svelte
@@ -0,0 +1,11 @@
+<div class="container">
+    {#if isImageVisible}
+      <img class="image visible" src="images/tardis.jpg" alt="Your Image" />
+    {:else}
+      <img class="image" src="images/tardis.jpg" alt="Your Image" />
+    {/if}
+    <div class="content">
+      <p>This is the text that becomes visible on hover.</p>
+    </div>
+  </div>
+  
