ae987efa390edaed4a678e2fe64abbec6743f830
Kyle Black <kdavidblack89@gmail.com>
2023-05-22 10:15:52 +1200

    add come-into-view styles

--- my-app/src/lib/come-into-view.svelte
+++ my-app/src/lib/come-into-view.svelte
@@ -8,4 +8,44 @@
       <p>This is the text that becomes visible on hover.</p>
     </div>
   </div>
+    
+  <style>
+    .container {
+      position: relative;
+      display: inline-block;
+      overflow: hidden;
+    }
+  
+    .content {
+      opacity: 0;
+      transform: translateY(100%);
+      position: absolute;
+      top: 100%;
+      left: 0;
+      padding: 0.5rem;
+      background-color: #f5f5f5;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      z-index: 1;
+      transition: opacity 0.3s ease, transform 0.5s ease;
+    }
+  
+    .container:hover .content {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  
+    .image {
+      opacity: 0;
+      transform: translateY(100%);
+      max-width: 80%;
+      height: auto;
+      transition: opacity 0.3s ease, transform 0.5s ease;
+    }
+  
+    .image.visible {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  </style>
   
