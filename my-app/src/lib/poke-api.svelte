18a8995aba0cdf729cd7ffd1b979bb05f07edc72
Kyle Black <kdavidblack89@gmail.com>
2023-05-23 13:24:26 +1200

    add css styling to the poki-api

---
+++ my-app/src/lib/poke-api.svelte
@@ -0,0 +1,34 @@
+
+  <title>Random Pokémon Image</title>
+ 
+ <style>
+    main {
+      text-align: center;
+      padding: 20px;
+    }
+  
+    h1 {
+      margin-bottom: 20px;
+    }
+  
+    img {
+      width: 200px;
+      height: 200px;
+      margin-bottom: 20px;
+    }
+  
+    button {
+      padding: 10px 20px;
+      font-size: 16px;
+    }
+  </style>
+  
+
+
+  <div class="container">
+    <h1>Random Pokémon Image</h1>
+    <img id="pokemonImage" src="" alt="Pokemon Image">
+    <button id="fetchButton">Fetch Random Pokémon</button>
+  </div>
+
+  <script src="script.js"></script>
