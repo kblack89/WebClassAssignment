39c25b90829af7e8d7b8b162fba4b0a09a54de85
Kyle Black <kdavidblack89@gmail.com>
2023-05-23 14:27:10 +1200

    add topcontent into its own componant

---
+++ my-app/src/lib/top-content-disp.svelte
@@ -0,0 +1,61 @@
+<div class="title-section ">
+  <h2 class = "TitlePt1">KYLE BLACK</h2>
+  <h5 class = "TitlePt2">A GUIDE TO EPICNESS!</h5>
+  <div class="titleimage"></div> 
+  <h5 class = "TitlePt3">The many Hobbies, Interests and Passsions I have.</h5>
+
+  </div>
+  <style>
+
+
+.TitlePt1
+{
+margin-right: 1200px;
+color:coral;
+animation: glow 1s ease-in-out infinite alternate;
+}
+
+.TitlePt2
+{
+margin-right: 1200px;
+color:coral;
+animation: glow 1s ease-in-out infinite alternate;
+}
+
+.TitlePt3
+{
+margin-left: 1000px;
+margin-top: -180px;
+color:coral;
+
+}
+
+
+
+
+.title-section {
+  background-color: rgba(0, 0, 0, 0.445);
+  flex: 70%;
+  padding: 80px;
+  text-align: center;
+  color: rgb(255, 255, 255);
+  font-size: 50PX;
+  height: 600px;
+  margin-left: 100px;
+  margin-right: 100px;
+}
+
+
+.titleimage {
+  background-image: url("/images/jokerme.png");
+  color: #aaa;
+  height:320px;
+  width:500px;
+  padding: 5px;
+  display: grid;
+  place-items: center;
+  margin:0 auto;
+  margin-top: -380px;
+}
+
+</style>
