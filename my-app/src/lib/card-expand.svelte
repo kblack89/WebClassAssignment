b26e14499ebca0ba1ea90709d244c14647fe34bf
Kyle Black <kdavidblack89@gmail.com>
2023-05-24 14:24:24 +1200

    Add componant code to make all cards not selected blur

---
+++ my-app/src/lib/card-expand.svelte
@@ -0,0 +1,63 @@
+<div id="card-container">
+    <div class="card">
+      <img src="images/tardis.jpg" alt="Image 1">
+    </div>
+    <div class="card">
+      <img src="images/tardis.jpg" alt="Image 2">
+    </div>
+    <div class="card">
+      <img src="images/tardis.jpg" alt="Image 3">
+    </div>
+    <!-- Repeat the above card structure for the remaining cards -->
+  </div>
+
+  <style>
+
+.card-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+}
+
+.card {
+  position: relative;
+  overflow: hidden;
+  transition: 0.3s;
+}
+
+.card img {
+  width: 10%;
+  height: 10%;
+  object-fit: cover;
+}
+
+.card:hover {
+  transform: scale(1.1);
+  z-index: 1;
+}
+
+.card:hover ~ .card {
+  filter: blur(4px);
+}
+  </style>
+
+  <script>
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    cards.forEach(c => {
+      if (c !== card) {
+        c.classList.add('blur');
+      }
+    });
+  });
+
+  card.addEventListener('mouseleave', () => {
+    cards.forEach(c => {
+      c.classList.remove('blur');
+    });
+  });
+});
+  </script>
